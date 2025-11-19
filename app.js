88eb448c7908b540bb7a41e52976eb2f30f75c85
Dung Nguyen <nguyenvandung26092004@gmail.com>
2025-11-19 14:40:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const express = require('express');
+const app = express();
+
 const PORT = 3000;
-const HOST = "0.0.0.0";  // Quan trọng!
+const HOST = '0.0.0.0';  // Quan trọng để Docker nhận kết nối từ ngoài
+
+app.get('/', (req, res) => {
+  res.send('Hello from Dockerized Node.js App!');
+});
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server running at http://${HOST}:${PORT}`);
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
