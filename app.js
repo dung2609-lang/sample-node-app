332669eb528ce31f8dd8b753902b1fa5234bca09
Dung Nguyen <nguyenvandung26092004@gmail.com>
2025-11-19 14:37:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-const express = require("express");
-const app = express();
+const PORT = 3000;
+const HOST = "0.0.0.0";  // Quan trọng!
 
-app.get("/", (req, res) => {
-  res.send("Demo CI/CD NodeJS OK!");
+app.listen(PORT, HOST, () => {
+    console.log(`Server running at http://${HOST}:${PORT}`);
 });
-
-app.listen(3000, () => console.log("App running on port 3000"));
